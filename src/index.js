5ef916ae163a563e1dfa3d23a7d29fd301af713a
github username <github email address>
2018-09-21 12:03:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 function handleSubmit(e) {
 
 }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
-  form.addEventListener("submit", handleSubmit)
+  form.addEventListener("submit", a => {
+    a.preventDefault()
+    let blockColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
+    // let defColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
+    let defColor = `rgb(255,255,255)`
+    for(let i = 0; i < 5; i++){
+      for(let j = 0; j < 3; j++){
+        if(getRandomInt(2) == 1){
+          document.getElementById(`${i}-${j}`).style.backgroundColor = blockColor
+          document.getElementById(`${i}-${4-j}`).style.backgroundColor = blockColor
+        }
+        else{
+          document.getElementById(`${i}-${j}`).style.backgroundColor = defColor
+          document.getElementById(`${i}-${4-j}`).style.backgroundColor = defColor
+        }
+      }
+    }
+
+  })
 })
