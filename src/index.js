07ddfd3bb4c22012c6765780f96fc15d8fc9f82b
github username <github email address>
2018-09-24 18:05:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+const form = document.getElementById("identicon-form")
+const text = document.getElementById("text-entry")
 function handleSubmit(e) {
-
+  e.preventDefault()
+  const identicon = new Identicon (text.value)
+  // alert(`Identicon's hash is: ${identicon.colorHash}`)
+  let squares = Identicon.getSquares()
+  let colors = identicon.getColors()
+  for (let key in squares){
+    document.getElementById(key).style.backgroundColor = colors[squares[key]]
+  }
+  // alert(`Text value is: ${text.value}\nHashed is: ${md5.array(text.value)}`)
 }
-
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
 })
