bb09e9b9c54fc6af1e1a5d1998119bb5be82f47a
joyhuangg <joyhuangg@gmail.com>
2018-09-25 14:19:07 -0400

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,13 @@
-function updateDOM() {
-
+function updateDOM(identicon) {
+  const grid = identicon.grid
+  for (let r = 0; r < 5; r++){
+    for (let c = 0; c < 5; c++){
+      if (grid[r][c] === 1){
+        document.getElementById(`${r}-${c}`).style.backgroundColor = identicon.color
+      }
+      else{
+        document.getElementById(`${r}-${c}`).style.backgroundColor = "rgb(280,280,280)"
+      }
+    }
+  }
 }
