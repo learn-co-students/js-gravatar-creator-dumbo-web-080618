b86bcbb0053c2cdfbd1f001be7bb8ee785825b79
Felix Chan <felixchan29@gmail.com>
2018-11-22 15:09:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", a => {
     a.preventDefault()
-    let blockColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
+    //let blockColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
     // let defColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
     let defColor = `rgb(255,255,255)`
+    let text_value = a.target[0].value
+    const hashedArr = md5.array(text_value)
+    let blockColor = `rgb(${hashedArr[0]},${hashedArr[1]},${hashedArr[2]})`
+    let c = 0
     for(let i = 0; i < 5; i++){
       for(let j = 0; j < 3; j++){
-        if(getRandomInt(2) == 1){
+        if(hashedArr[c] < 127){
           document.getElementById(`${i}-${j}`).style.backgroundColor = blockColor
           document.getElementById(`${i}-${4-j}`).style.backgroundColor = blockColor
         }
@@ -22,6 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
           document.getElementById(`${i}-${j}`).style.backgroundColor = defColor
           document.getElementById(`${i}-${4-j}`).style.backgroundColor = defColor
         }
+        c+=1
       }
     }
 
